51ec69a790ae41c941aa3adbd97b43660010e21a
ShuN01114 <shunta.third@gmail.com>
2023-04-17 00:02:46 +0900

    上下左右をひっくり返せるようになったよ

--- OseroMethods.java
+++ OseroMethods.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class OseroMethods {
 
     int board_size = 8; //８×８の盤面
     String[][] board = new String[board_size][board_size];
+    String black ="⚫︎";
+    String white = "⚪︎"; 
 
     public OseroMethods(){
         init();
@@ -14,10 +18,10 @@ public class OseroMethods {
                 board[i][j] = "　";
             }
         }
-        board[3][3] = "⚫︎";
-        board[4][4] = "︎⚫︎";
-        board[3][4] = "⚪︎";
-        board[4][3] = "⚪︎";
+        board[3][3] = black;
+        board[4][4] = black;
+        board[3][4] = white;
+        board[4][3] = white;
     }
 
     public void seeBoard(){ //ターミナル上で盤面を確認するため
@@ -36,50 +40,95 @@ public class OseroMethods {
     }
     public void setStone(int player, int x, int y){ //駒を置く
         if(player == 1){
-            board[x][y] = "⚫︎";
+            board[x][y] = black;
         } else if(player == 2){
-            board[x][y] = "⚪︎";
+            board[x][y] = white;
         } else{
             System.out.println("プレイヤーの指定が間違っています");
         }
         reverseStone(player, x, y);
     }
     public void reverseStone(int player, int x, int y){ //駒をひっくり返す
-        String playerStone = "";
-        String enemyStone = "";
+        String playerStone = "", enemyStone= "";
+
         if(player == 1){
-            playerStone = "⚫︎";
-            enemyStone = "⚪︎";
+            playerStone = black;
+            enemyStone = white;
         } else if(player == 2){
-            playerStone = "⚪︎";
-            enemyStone = "⚫︎";
+            playerStone = white;
+            enemyStone = black;
         }
-        reverseTop(playerStone, enemyStone, x, y);//左側のコマをひっくり返す
-    }
-    public void reverseTop(String playerStone, String enemyStone, int x, int y){ //左側のコマをひっくり返す
-        for(int i = x-1; i > 0; i--){
-            if(board[i][y]==enemyStone){
+        
+        for(int i = x+1; i < board_size; i++){//上方向の駒をひっくり返す
+            if(board[i][y].equals(enemyStone)){
                 continue;
-            }else if(board[i][y]==playerStone){
+            }else if(board[i][y].equals(playerStone)){
+                for(i-=1; i>x; i--){
+                    board[i][y] = playerStone;
+                }
+            }
+            break;
+        }
+
+        for(int i = y+1; i < board_size; i++){//右方向の駒をひっくり返す
+            if(board[x][i].equals(enemyStone)){
+                continue;
+            }else if(board[x][i].equals(playerStone)){
+                for(i-=1; i>y; i--){
+                    board[x][i] = playerStone;
+                }
+            }
+            break;
+        }
+
+        for(int i = x-1; i > 0; i--){//下方向の駒をひっくり返す
+            if(board[i][y].equals(enemyStone)){
+                continue;
+            }else if(board[i][y].equals(playerStone)){
                 for(i+=1; i<x; i++){
                     board[i][y] = playerStone;
                 }
-                break;
             }
+            break;
+        }
+
+        for(int i = y-1; i > 0; i--){//左方向の駒をひっくり返す
+            if(board[x][i].equals(enemyStone)){
+                continue;
+            }else if(board[x][i].equals(playerStone)){
+                for(i+=1; i<y; i++){
+                    board[x][i] = playerStone;
+                }
+            }
+            break;
         }
     }
 
+    public void debMode(OseroMethods osero){
+        osero.seeBoard();
+        Scanner s = new Scanner(System.in);
+        while(true){
+            System.out.print("駒をおくx座標を入力してください:");
+            int x = s.nextInt();
+            System.out.print("駒をおくy座標を入力してください:");
+            int y = s.nextInt();
+            osero.setStone(1, x, y);
+            osero.seeBoard();
+
+            System.out.print("駒をおくx座標を入力してください:");
+            x = s.nextInt();
+            System.out.print("駒をおくy座標を入力してください:");
+            y = s.nextInt();
+            osero.setStone(2, x, y);
+            osero.seeBoard();
+        }
+    }
+
+
     public static void main(String[] args) {
         OseroMethods osero = new OseroMethods();
-        osero.seeBoard();
-        osero.setStone(1, 4, 2);
-        osero.seeBoard();
-        osero.setStone(2, 3, 2);
-        osero.seeBoard();
-        osero.setStone(1, 5, 3);
-        osero.seeBoard();
-        osero.setStone(2, 5, 4);
-        osero.seeBoard();
+        
+        osero.debMode(osero);
     }
 
 }
