1e0f5ff06a51f5333c8880a36dc7392d32f81afa
杉本陽紀 <harusugi@Harusugis-Mac.local>
2023-05-12 18:41:47 +0900

    GUI大きく変えた

--- OseroGui.java
+++ OseroGui.java
@@ -1,10 +1,26 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 import java.io.*;
-import java.net.*;
-
-public class OseroGui extends JFrame implements ActionListener{
+public class OseroGui {
+    static int width = 500;
+    static int height = 550;
+    int lm = 50;    // 左側余白
+    int tm = 100;   // 上側余白
+    int cs = 50;  //マスのサイズ
+    CardLayout cardLayout;
+    JPanel containerPanel;
+    JFrame mainFrame;
+    int ban[][] = {
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,1,2,0,0,0},
+        {0,0,0,2,1,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+    };
     JLabel announce;
     JLabel partner;
     JButton button1;
@@ -23,122 +39,49 @@ public class OseroGui extends JFrame implements ActionListener{
     JPanel login_page;
     JPanel signup_page;
     JPanel standby_page;
-    JPanel osero_page;
-
 
     OseroGui(Client cl){
-        OseroGui gui = new OseroGui("オセロ", cl);
-        gui.setVisible(true); //可視化
-    }
-
-    OseroGui(String title, Client cl){
         client = cl;
-        setTitle(title);
-        setSize(500, 300); //大きさ指定
-        setLocationRelativeTo(null); //GUIを中央に配置
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-
-        //firstpage
+        mainFrame = new JFrame("オセロアプリ");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(width, height);
+        containerPanel = new JPanel();
+        cardLayout = new CardLayout();
+        containerPanel.setLayout(cardLayout);
         first_page();
-            
-        //loginpage
         login_page();
-
-        //signuppage
         signup_page();
-
-        //standbypage
         standby_page();
-
-        //oseropage
-        osero_page();
-
-        card_panel = new JPanel();
-        layout = new CardLayout();
-        card_panel.setLayout(layout);
-        card_panel.add(first_page);
-        card_panel.add(login_page);
-        card_panel.add(signup_page);
-        card_panel.add(standby_page);
-        card_panel.add(osero_page);
-
-        getContentPane().add(card_panel, BorderLayout.CENTER);
+        containerPanel.add(first_page, "first");
+        containerPanel.add(login_page, "login");
+        containerPanel.add(signup_page, "signup");
+        containerPanel.add(standby_page, "standby");
+        containerPanel.add(new Reversi(), "osero");
+        mainFrame.getContentPane().add(containerPanel, BorderLayout.CENTER);
+        // mainFrame.pack();
+        mainFrame.setVisible(true);
     }
-
-    public void actionPerformed(ActionEvent e){
-        if (e.getSource() == button1) {//first -> signup
-            layout.next(card_panel);
-            layout.next(card_panel);
-        } else if (e.getSource() == button2){ //first -> login
-            layout.next(card_panel);
-        } else if (e.getSource() == button3){ //login -> standby
-            String name = login_name.getText();
-            char[] password = login_pass.getPassword();
-            String pass = new String(password);
-            client.out.println("login");
-            client.out.println(name);
-            client.out.println(pass);
-            try{
-                String ans = client.in.readLine();
-                if(ans.equals("SUCCESS")){
-                    layout.next(card_panel);
-                    layout.next(card_panel);
-                } else {
-                    JFrame Error_frame = new JFrame();
-                    JOptionPane.showMessageDialog(Error_frame, "登録されていない，もしくはパスワードが違う");
-                }
-            }catch(IOException er){
-
-            }
-        } else if (e.getSource() == button4){ //signup -> standby
-            String name = signup_name.getText();
-            char[] password = signup_pass.getPassword();
-            String pass = new String(password);
-            client.out.println("signup");
-            client.out.println(name);
-            client.out.println(pass);
-            try{
-                String ans = client.in.readLine();
-                if(ans.equals("SUCCESS")){
-                    layout.next(card_panel);
-                } else {
-                    JFrame Error_frame = new JFrame();
-                    JOptionPane.showMessageDialog(Error_frame, "登録済み");
-                }
-            }catch(IOException er){
-
-            }
-        } else if (e.getSource() == access){
-            announce.setText("対戦相手待ち");
-            announce.paintImmediately( announce.getVisibleRect());
-            GameStart();
-
-        }
-    }
-    
-    public void GameStart(){
-        client.out.println("gamestart"); 
-        layout.next(card_panel);
-        try {
-            String partner_name = client.in.readLine();
-            String text = "対戦相手：" + partner_name;
-            partner.setText(text);
-        } catch (IOException er) {
-            er.printStackTrace();
-        }
-    }
-
     public void first_page(){
         first_page = new JPanel();
         first_page.setLayout(new GridBagLayout());
         first_page.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         button1 = new JButton("SignUp");
         button1.setPreferredSize(new Dimension(150, 50));
-        button1.addActionListener(this);
+        button1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                cardLayout.show(containerPanel, "signup");
+            }
+        });
+
         button2 = new JButton("Login");
         button2.setPreferredSize(new Dimension(150, 50));
-        button2.addActionListener(this);
+        button2.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                cardLayout.show(containerPanel, "login");
+            }
+        });
         first_page.add(button1);
         first_page.add(button2);
     }
@@ -149,7 +92,29 @@ public class OseroGui extends JFrame implements ActionListener{
         JLabel name = new JLabel("ユーザー名：");
         JLabel pass = new JLabel("パスワード：");
         button3 = new JButton("Login");
-        button3.addActionListener(this);
+        button3.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                cardLayout.show(containerPanel, "standby");
+                String name = login_name.getText();
+                char[] password = login_pass.getPassword();
+                String pass = new String(password);
+                client.out.println("login");
+                client.out.println(name);
+                client.out.println(pass);
+                try{
+                    String ans = client.in.readLine();
+                    if(ans.equals("SUCCESS")){
+                        cardLayout.show(containerPanel, "standby");
+                    } else {
+                        JFrame Error_frame = new JFrame();
+                        JOptionPane.showMessageDialog(Error_frame, "登録されていない，もしくはパスワードが違う");
+                    }
+                }catch(IOException er){
+
+                }
+            }
+        });
         login_page.add(name);
         login_page.add(login_name);
         login_page.add(pass);
@@ -161,7 +126,28 @@ public class OseroGui extends JFrame implements ActionListener{
         signup_name = new JTextField(10);
         signup_pass = new JPasswordField(10);
         button4 = new JButton("SignUp");
-        button4.addActionListener(this);
+        button4.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                String name = signup_name.getText();
+                char[] password = signup_pass.getPassword();
+                String pass = new String(password);
+                client.out.println("signup");
+                client.out.println(name);
+                client.out.println(pass);
+                try{
+                    String ans = client.in.readLine();
+                    if(ans.equals("SUCCESS")){
+                        cardLayout.show(containerPanel, "standby");
+                    } else {
+                        JFrame Error_frame = new JFrame();
+                        JOptionPane.showMessageDialog(Error_frame, "登録済み");
+                    }
+                }catch(IOException er){
+
+                }  
+            }
+        });
         signup_page.add(signup_name);
         signup_page.add(signup_pass);
         signup_page.add(button4);
@@ -170,15 +156,71 @@ public class OseroGui extends JFrame implements ActionListener{
         standby_page = new JPanel();
         announce = new JLabel("　　　　　　　");//とりまこれで動いたからそのまま
         access = new JButton("接続");
-        access.addActionListener(this);
+        access.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                client.out.println("gamestart"); 
+                cardLayout.show(containerPanel, "osero");
+            }
+        });
         standby_page.add(announce);
         standby_page.add(access);
     }
-    public void osero_page(){
-        osero_page = new JPanel();
-        partner = new JLabel("");
-        osero_page.add(partner);
-        //ここにオセロの盤面を作成
-    }
+    public class Reversi extends JPanel {    
+        // コンストラクタ（初期化処理）
+        public Reversi() {
+            setPreferredSize(new Dimension(WIDTH,HEIGHT));
+            addMouseListener(new MouseProc());
+        }
+     
+        // 画面描画
+        public void paintComponent(Graphics g) {
+            // 背景
+            g.setColor(Color.gray);
+            g.fillRect(0, 0, WIDTH, HEIGHT);
+            // 盤面描画
+            for (int i = 0; i < 8; i++) {
+                int y = tm + cs * i;
+                for (int j = 0; j < 8; j++) {
+                    int x = lm + cs * j;
+                    g.setColor(new Color(0, 170, 0));
+                    g.fillRect(x, y, cs, cs);
+                    g.setColor(Color.black);
+                    g.drawRect(x, y, cs, cs);
+                    if (ban[i][j] != 0) {
+                        if (ban[i][j] == 1) {
+                            g.setColor(Color.black);
+                        } else {
+                            g.setColor(Color.white);
+                        }
+                        g.fillOval(x+cs/10, y+cs/10, cs*8/10, cs*8/10);
+                    }
+                }
+            }
+        }
+     
+        // クリックされた時の処理用のクラス
+        class MouseProc extends MouseAdapter {
+            public void mouseClicked(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                // 盤の外側がクリックされたときは何もしないで終了
+                if (x < lm) return;
+                if (x >= lm+cs*8) return;
+                if (y < tm) return;
+                if (y >= tm+cs*8) return;
+                // クリックされたマスを特定
+                int row = (y - tm) / cs;
+                int col = (x - lm) / cs;
+                if (ban[row][col] == 0) {
+                    // 黒コマを置く
+                    ban[row][col] = 2; //白か黒固定
 
+                }
+                // 再描画
+                repaint();
+            }
+        }
+
+    }
 }
