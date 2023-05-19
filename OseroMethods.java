862119d0bcd9a95f631f8c0a49012a650d2af25b
ShuN01114 <shunta.third@gmail.com>
2023-05-19 09:59:42 +0900

    ひっくり返せるのがあるかとどっちの勝ちか判定できるようにした

--- OseroMethods.java
+++ OseroMethods.java
@@ -9,6 +9,120 @@ public class OseroMethods {
         this.board = board;
     }
 
+    public int checkStone(int player){//playerが置ける場所が存在するかチェックする
+        int playerStone = 0, enemyStone = 0;
+        int count = 0;
+        if(player == 1){
+            playerStone = 1;
+            enemyStone = 2;
+        } else if(player == 2){
+            playerStone = 2;
+            enemyStone = 1;
+        }
+        for(int i = 0; i < board_size; i++){
+            for(int j = 0; j < board_size; j++){
+                if(board[i][j] == enemyStone){
+                    continue;
+                } else if(board[i][j] == playerStone){
+                    for(int k = i-1; k > 0; k--){//上方向
+                        if(board[k][j] == enemyStone){
+                            continue;
+                        } else if(board[k][j] == 0){
+                            count++;
+                            break;
+                        }
+                        break;
+                    }
+                    for(int k = i-1, l = j+1; k > 0 && l < board_size; k--, l++){//右上方向
+                        if(board[k][l] == enemyStone){
+                            continue;
+                        } else if(board[k][l] == 0){
+                            count++;
+                            break;
+                        }
+                        break;
+                    }
+                    for(int k = j+1; k < board_size; k++){//右方向
+                        if(board[i][k] == enemyStone){
+                            continue;
+                        } else if(board[i][k] == 0){
+                            count++;
+                            break;
+                        }
+                        break;
+                    }
+                    for(int k = i+1, l = j+1; k < board_size && l < board_size; k++, l++){//右下方向
+                        if(board[k][l] == enemyStone){
+                            continue;
+                        } else if(board[k][l] == 0){
+                            count++;
+                            break;
+                        }
+                        break;
+                    }
+                    for(int k = i+1; k < board_size; k++){//下方向
+                        if(board[k][j] == enemyStone){
+                            continue;
+                        } else if(board[k][j] == 0){
+                            count++;
+                            break;
+                        }
+                        break;
+                    }
+                    for(int k = i+1, l = j-1; k < board_size && l > 0; k++, l--){//左下方向
+                        if(board[k][l] == enemyStone){
+                            continue;
+                        } else if(board[k][l] == 0){
+                            count++;
+                            break;
+                        }
+                        break;
+                    }
+                    for(int k = j-1; k > 0; k--){//左方向
+                        if(board[i][k] == enemyStone){
+                            continue;
+                        } else if(board[i][k] == 0){
+                            count++;
+                            break;
+                        }
+                        break;
+                    }
+                    for(int k = i-1, l = j-1; k > 0 && l > 0; k--, l--){//左上方向
+                        if(board[k][l] == enemyStone){
+                            continue;
+                        } else if(board[k][l] == 0){
+                            count++;
+                            break;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println("count = " + count);//デバッグ用 count=0のとき置ける場所がない
+        return count;
+    }
+    public int scoreStone(){//駒の数を数える
+        int black = 1, white = 2;
+        for(int i = 0; i < board_size; i++){
+            for(int j = 0; j < board_size; j++){
+                if(board[i][j] == 1){
+                    black++;
+                } else if(board[i][j] == 2){
+                    white++;
+                }
+            }
+        }
+        System.out.println("黒 = " + black + " 白 = " + white);//デバッグ用
+        if(black > white){
+            return 1;//黒の勝ち
+        } else if(black < white){
+            return 2;//白の勝ち
+        } else{
+            return 0;//引き分け
+        }
+    }
+
     public int setStone(int player, int x, int y){ //駒を置く
         // if((board[x][y].equals(black))||(board[x][y].equals(white))){//すでにコマがある場合
         //     return -1;//置けなかったことを表す
@@ -139,6 +253,8 @@ public class OseroMethods {
         //     osero.setStone(2, x, y);
         // }
         int check = setStone(player, x, y);
+        checkStone(player%2+1);//デバッグ用
+        System.out.println(scoreStone());//デバッグ用
         return check;
     }
      public int[][] get_Board(){
